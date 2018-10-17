52c23a1e07332bea11ee5058091cc4a1f586f44b
izmirtais <izmirtais28@gmail.com>
2018-10-17 18:24:06 +0700

    minggu 8

---
+++ src/cobaminggu8/Cobaminggu8.java
@@ -0,0 +1,16 @@
+package cobaminggu8;
+
+/**
+ *
+ * @author Ismir
+ */
+public class Cobaminggu8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
