f23131077b771329551e419efa8fb19e82341083
izmirtais <izmirtais28@gmail.com>
2018-10-17 18:49:02 +0700

    isi

--- src/cobaminggu8/Cobaminggu8.java
+++ src/cobaminggu8/Cobaminggu8.java
@@ -10,7 +10,7 @@ public class Cobaminggu8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello Gaes");
     }
     
 }
